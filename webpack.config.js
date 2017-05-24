8ad75e8a01a78041bf384c42fe9e08257226997e
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2017-05-24 22:43:22 +0200

    Added webpack

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+
+module.exports = {
+  entry: ['aframe', './index.js'],
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+}
