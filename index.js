15ab759a4e3eb2877ff1067e220c34b8e7167f44
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2017-05-26 21:48:32 +0200

    Moving cubes!

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-  const light = document.querySelector('#light')
-  console.log(light)
+import AFRAME from 'aframe'
+
+AFRAME.registerComponent('move-around', {
+  init: function () {
+    let counter = 0
+
+    let {x, y, z} = this.el.getAttribute('position')
+
+    const step = () => {
+      counter += 0.05
+
+      let newX = x * Math.cos(counter)
+      let newY = y
+      let newZ = z * Math.sin(counter)
+
+      this.el.setAttribute('position', { x: newX, y: newY, z: newZ })
+
+      window.requestAnimationFrame(step)
+    }
+
+    window.requestAnimationFrame(step)
+  }
 })
