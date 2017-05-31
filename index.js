44fcab8e1cb6e986eb841bcf7bb0e5126feafb46
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2017-05-31 20:01:36 +0200

    Added on click handler

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 import AFRAME from 'aframe'
 
+const CODING_TOS = {
+  violini: 'work out',
+  strazzullo: 'enjoy',
+  dellava: 'create',
+  dluconi: 'rock',
+  mluconi: 'disrupt',
+  mandolini: 'make'
+}
+
 const START_X = -4
-const START_Y = 3
-const Z = -3
+const START_Y = 2
+const Z = -4
 const WIDTH = 1
 const SPACING = 0.5
 const ELEMENTS_IN_A_ROW = 6
 
 let index = 0
+
+const onExerClick = exer => {
+  const codingTo = `coding to ${CODING_TOS[exer]}`
+  console.log(codingTo)
+}
 
 AFRAME.registerComponent('exer', {
   schema: {type: 'string'},
@@ -18,6 +32,8 @@ AFRAME.registerComponent('exer', {
     this.el.setAttribute('width', WIDTH)
     this.el.setAttribute('height', WIDTH)
     this.el.setAttribute('material', {src: `#${this.data}`})
+
+    this.el.addEventListener('click', () => { onExerClick(this.data) })
 
     index++
   }
