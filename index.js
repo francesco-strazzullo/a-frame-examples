4628bfcd58e56a37aac59e9d687181b85917cc45
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2017-05-31 19:49:40 +0200

    added all exers foto

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ const START_Y = 3
 const Z = -3
 const WIDTH = 1
 const SPACING = 0.5
+const ELEMENTS_IN_A_ROW = 6
 
 let index = 0
 
 AFRAME.registerComponent('exer', {
   schema: {type: 'string'},
   init: function () {
-    const x = START_X + ((WIDTH + SPACING) * index)
-    this.el.setAttribute('position', {x, y: START_Y, z: Z})
+    const x = START_X + ((WIDTH + SPACING) * (index % ELEMENTS_IN_A_ROW))
+    const y = START_Y - ((WIDTH + SPACING) * Math.floor(index / ELEMENTS_IN_A_ROW))
+    this.el.setAttribute('position', {x, y, z: Z})
     this.el.setAttribute('width', WIDTH)
     this.el.setAttribute('height', WIDTH)
     this.el.setAttribute('material', {src: `#${this.data}`})
